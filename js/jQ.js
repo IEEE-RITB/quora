ac843a7f9e4f19a9afb651579670422659e44c0e
IEEERITB <31928952+IEEERITB@users.noreply.github.com>
2017-10-18 23:25:19 +0530

    jQ.js_updated

--- js/jQ.js
+++ js/jQ.js
@@ -4,15 +4,19 @@ $(document).ready(function(){
 var size= $("#CDTimer").width()*0.09;
 var size2= $("#CDTimer").width()*0.04;
 var size3,size4;
-var size_cover=$("#section_1").width();
+var menu_font;
+var size_cover=$("#section_1").width()*0.45;
+
 $("#timer").css("font-size",size);
 $("#message").css("font-size",size2);
-$("#f1").css("width",size_cover);
+$("#section_1").css("height",size_cover);
 $("#menu2").hide(); $("#logo2").hide();
 if ($(window).width() < 768) {
 	size3= $(".sec_head").width()*0.09;
     size4= $(".sec_p").width()*0.05;
-    $("#CDTimer").css("top","65%");
+    menu_font=$("#menu div").width()*0.2;
+    $("#menu div").css({"font-size":menu_font,"text-shadow":"1px 1px 3px #b92b27, 0 0 10px #b92b27, 0 0 2px #b92b27"});
+    $("#CDTimer").css("top","45%");
 	$(".cont").css({"padding":"5px 0px","font-size":"12px"});
 	$("timer").css("font-size","30px");
 	$("message").css("font-size","8px");
@@ -20,8 +24,9 @@ if ($(window).width() < 768) {
 	$(".table").css("font-size","10px");
 
  //  alert('Less than 960');
- $("#menu").hide(); $("#logo").hide();
- $("#menu2").show(); $("#logo2").show();
+ $("#menu").show(); $("#logo").hide();
+ $("#menu2").hide(); $("#logo2").show();
+ 
  $("#menu2").click(function(){
  	$("#menu").toggle();
  })
@@ -29,6 +34,8 @@ if ($(window).width() < 768) {
 else{
 	size3= $(".sec_head").width()*0.06;
     size4= $(".sec_p").width()*0.024;
+    menu_font=$("#menu div").width()*0.17;
+    $("#menu div").css({"font-size":menu_font,"text-shadow": "5px 5px 10px #b92b27, 0 0 25px #b92b27, 0 0 5px #b92b27"});
     $(".cont").css({"font-size":"15px","padding":"30px"});
 }
 $(".sec_head").css("font-size",size3);
